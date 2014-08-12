c76f661038f9043a36db3f6d0ecb7d1170b94cc8
Gabriel Perdue <perdue@fnal.gov>
2014-08-12 14:55:09 -0500

    First commit.

---
+++ analyze_sample_scan.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+"""
+analyze_sample_scan scanfile.log
+
+This program assumes the scanfile.log was produced by the run_validation.sh
+script. This means the content of the file is expected to look like so:
+
+    Analyzing file gntp.1002.ghep.root...
+    --------------------
+    1407444969 ERROR gvldtest : [n] <gVldSampleScan.cxx::main (209)> : \
+        ** s is not conserved at this event
+    1407444969 ERROR gvldtest : [n] <gVldSampleScan.cxx::main (212)> :
+    1407444969 ERROR gvldtest : [n] <gVldSampleScan.cxx::main (213)> :
+"""
+
+import sys
+import re
+
+if '-h' in sys.argv or '--help' in sys.argv:
+    print __doc__
+    sys.exit(1)
+
+if not len(sys.argv) == 2:
+    print 'The scanfile log argument is mandatory.'
+    print __doc__
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+run_dictionary = {
+    '1000': {'evts': 100000, 'desc': 'numu    + n',
+             'energy': 0.5, 'channels': 'all'},
+    '1001': {'evts': 100000, 'desc': 'numu    + n',
+             'energy':   1, 'channels': 'all'},
+    '1002': {'evts': 100000, 'desc': 'numu    + n',
+             'energy':   5, 'channels': 'all'},
+    '1003': {'evts': 100000, 'desc': 'numu    + n',
+             'energy':  50, 'channels': 'all'},
+    '1101': {'evts': 100000, 'desc': 'numubar + p',
+             'energy':   1, 'channels': 'all'},
+    '1102': {'evts': 100000, 'desc': 'numubar + p',
+             'energy':   5, 'channels': 'all'},
+    '1103': {'evts': 100000, 'desc': 'numubar + p',
+             'energy':  50, 'channels': 'all'},
+    '2001': {'evts': 100000, 'desc': 'numu    + Fe56',
+             'energy':  1, 'channels': 'all'},
+    '2002': {'evts': 100000, 'desc': 'numu    + Fe56',
+             'energy':  5, 'channels': 'all'},
+    '2003': {'evts': 100000, 'desc': 'numu    + Fe56',
+             'energy': 50, 'channels': 'all'},
+    '2101': {'evts': 100000, 'desc': 'numubar + Fe56',
+             'energy':  1, 'channels': 'all'},
+    '2102': {'evts': 100000, 'desc': 'numubar + Fe56',
+             'energy':  5, 'channels': 'all'},
+    '2103': {'evts': 100000, 'desc': 'numubar + Fe56',
+             'energy': 50, 'channels': 'all'},
+    '9001': {'evts': 100000, 'desc': 'numu    + Fe56',
+             'energy':  5, 'channels': 'DIS charm'},
+    '9002': {'evts': 100000, 'desc': 'numu    + Fe56',
+             'energy':  5, 'channels': 'QEL charm'},
+    '9101': {'evts': 100000, 'desc': 'numu    + Fe56',
+             'energy':  2, 'channels': 'COH CC+NC'},
+    '9201': {'evts': 100000, 'desc': 'nue     + Fe56',
+             'energy':  1, 'channels': 've elastic'},
+    '9202': {'evts': 100000, 'desc': 'numu    + Fe56',
+             'energy':  1, 'channels': 've elastic'},
+    '9203': {'evts':  50000, 'desc': 'numu    + Fe56',
+             'energy': 20, 'channels': 'IMD'},
+    '9204': {'evts':  50000, 'desc': 'nuebar  + Fe56',
+             'energy': 20, 'channels': 'IMD (annihilation)'}
+}
+
+results = {}
+filenum = 0
+
+with open(filename, 'r') as f:
+
+    for line in f:
+        line = line.rstrip()
+
+        if re.match("Analyzing file", line):
+            words = line.split()
+            gntp = words[2]
+            filenum = gntp.split('.')[1]
+
+        if re.search("is not conserved", line):
+            error = line.split(':')[-1]
+            if error in results:
+                d = results[error]
+                if filenum in d:
+                    d[filenum] += 1
+                else:
+                    d[filenum] = 1
+            else:
+                results[error] = {filenum: 1}
+
+
+print results
+
+for error in results:
+    nerror = 0
+    nevt = 0
+    for run in results[error]:
+        runnum = run
+        nerror += results[error][run]
+        if runnum in run_dictionary:
+            descr = run_dictionary[runnum]
+            nevt += descr['evts']
+            print "%s: %d / %d = %f" % \
+                (error, nerror, nevt, float(nerror)/nevt)
---
+++ xml2txt.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+# This script reads in a GENIE xml spline file, assuming one channel
+# and neutrino and antineutrino entries (muon type) and parses them
+# into two text files for easier plotting with Python / R.
+
+import sys
+import re
+import os
+
+
+def get_filenames(txt_name):
+    """
+    Take a base name (no extension) and add the nu / nu-bar designation
+    plus .txt. Check to see if the files exist and remove them if they
+    do (since we will be appending).
+    """
+    nu_name = txt_name + "_nu.txt"
+    nubar_name = txt_name + "_nubar.txt"
+
+    if os.path.isfile(nu_name):
+        os.remove(nu_name)
+
+    if os.path.isfile(nubar_name):
+        os.remove(nubar_name)
+
+    return nu_name, nubar_name
+
+
+def transform(xml_file_name):
+    """
+    Take the xml file name (basically, assume we're working in the same
+    directory - there are no path handling bits) and read through it.
+    If we find we're in a (muon) neutrino section, append to the muon
+    neutrino file. If we're in an antineutrino section (again, muon),
+    append to the antinu file.
+
+    Splitting structure generally assumes a GENIE xml file circa 2.8.
+    """
+    with open(xml_file_name, "r") as xml_file:
+
+        nu_name, nubar_name = get_filenames(xml_file_name.split(".")[0])
+
+        is_neutrino = False
+        is_antineutrino = False
+        for line in xml_file:
+
+            # nu vs nubar is identified in the "spline" block
+            if re.match("<spline", line):
+                pieces = line.split("/")
+                labels = pieces[2].split(";")
+                nuflav = labels[0].split(":")
+
+                if nuflav[1] == '14':
+                    is_neutrino = True
+                    is_antineutrino = False
+                    with open(nu_name, "a") as myfile:
+                        myfile.write("#neutrino\n")
+                        myfile.write("Energy CrossSection\n")
+
+                    myfile.close()
+
+                if nuflav[1] == '-14':
+                    is_neutrino = False
+                    is_antineutrino = True
+                    with open(nubar_name, "a") as myfile:
+                        myfile.write("#antineutrino\n")
+                        myfile.write("Energy CrossSection\n")
+
+                    myfile.close()
+
+            if re.match("(.*)<knot", line):
+                parts = line.split()
+                vals = parts[2] + " " + parts[5] + "\n"
+
+                if (is_neutrino):
+                    with open(nu_name, "a") as myfile:
+                        myfile.write(vals)
+
+                    myfile.close()
+
+                if (is_antineutrino):
+                    with open(nubar_name, "a") as myfile:
+                        myfile.write(vals)
+
+                    myfile.close()
+
+    xml_file.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "This script requires a filename argument."
+        sys.exit(2)
+
+    transform(sys.argv[1])
