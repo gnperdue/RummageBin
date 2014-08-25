22554b747af5e2c29e558c860daa0f4d0424c7c4
Gabriel Perdue <perdue@fnal.gov>
2014-08-25 11:18:29 -0500

    A couple of tricks for pulling numbers out of strings.

---
+++ get_ver_nums.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LHAPDFSRC=LHAPDF-6.1.4.tar.gz
+LHAPDFMAJOR=`echo $LHAPDFSRC | cut -c8-8`
+echo $LHAPDFMAJOR
+BOOSTSRC=boost_1_56_0.tar.gz
+BOOSTVER=`echo $BOOSTSRC | cut -c7-12`
+echo $BOOSTVER
+BOOSTVER=`echo $BOOSTSRC | python -c "import sys;t=sys.stdin.readline().split('.')[0].split('_');print '%s.%s.%s'%(t[1],t[2],t[3])"`
+echo $BOOSTVER
+
