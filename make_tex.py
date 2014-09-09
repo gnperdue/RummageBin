7214c1356320b2b42b0e2885e4c7bfe772abd338
Gabriel Perdue <perdue@fnal.gov>
2014-09-09 13:50:29 -0500

    Fix Excel csv to latex table maker to include table rules and print the dollar symbol correctly for a latex file.

--- make_tex.py
+++ make_tex.py
@@ -25,12 +25,22 @@ filename = sys.argv[1]
 # use the universal newline open (being deprecated in Python 3).
 ll = list(csv.reader(open(filename, 'rU'), delimiter=','))
 
-for line in ll:
+
+for i in range(len(ll)):
+    if i == 0:
+        print "\\toprule"
+    else:
+        print "\\midrule"
+
+    line = ll[i]
     stmnt = ''
     for word in line:
-        stmnt += word + ' & '
+        # build the split and swap $ for currency with escaped latex
+        stmnt += word.replace('$', '\\$') + ' & '
     # strings are immutable so make a new one that drops the last two
     # chars ('& ') and then add the latex "eol" (\\).
     new_stmnt = stmnt[:-2]
     new_stmnt += '\\\\'
     print new_stmnt
+
+print "\\bottomrule"
