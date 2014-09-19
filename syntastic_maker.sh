917d1b2815d416d153b86427654ef523dc8089cf
Gabriel Perdue <gnperdue@me.com>
2014-09-19 11:23:36 -0500

    Add a script that takes the Minerva software environment and gets the include paths for Syntastic.

---
+++ syntastic_maker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This script uses the Minerva software environment to create an include paths file for Syntastic.
+#  https://github.com/scrooloose/syntastic
+# First it gets all the "MINERVA" environment variables, then it filters out all non "ROOT" variables 
+# (Gaudi convention), then it excludes the PATH, and finally it pipes the output through a perl command
+# that splits on the '=' and drops the first part (the actual environment variable name itself).
+
+# First, remove any existing syntastic config files.
+rm -f .syntastic_cpp_config
+# Next, create our new config file.
+printenv | grep MINERVA | grep ROOT | grep -v PATH | perl -ne 'while (<>){@l=split("=",$_);print "-I".@l[1];}' >> .syntastic_cpp_config
