2cd35ac8ff7fdaf7a573bb7eabc237bcc8917352
Gabriel Perdue <perdue@fnal.gov>
2014-10-21 17:03:55 -0500

    Add a couple of checks to the exists checker.

--- exists.sh
+++ exists.sh
@@ -7,14 +7,30 @@ fi
 
 if [[ -e "$FILENAME" ]]; then
   echo "$FILENAME exists..."
+else # == if [[ ! -e "$FILENAME" ]]; then ...
+  echo "$FILENAME does not exist."
 fi
 
 if [[ -f "$FILENAME" ]]; then
   echo "$FILENAME is a regular file..."
+else
+  echo "$FILENAME is not a regular file."
 fi
 
 if [[ -s "$FILENAME" ]]; then
   echo "$FILENAME is not zero size..."
+else
+  echo "$FILENAME is zero size."
 fi
 
+if [[ -h "$FILENAME" ]]; then
+  echo "$FILENAME is a symbolic link..."
+else
+  echo "$FILENAME is not a symbolic link."
+fi
 
+if [[ -d "$FILENAME" ]]; then
+  echo "$FILENAME is a directory..."
+else
+  echo "$FILENAME is not a directory."
+fi
