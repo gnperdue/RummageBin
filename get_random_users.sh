2928448d958eb82b00a18339d081326f28f1b2d3
Gabriel Perdue <gnperdue@me.com>
2014-11-21 11:18:58 -0600

    Do some more stuff

--- get_random_users.sh
+++ get_random_users.sh
@@ -29,10 +29,10 @@ if [[ $CHATTY == "true" ]]; then
 fi
 
 # Just get a few fields 
-< ${FILEBASE}.json jq -r "[.results[] | {first: .user.name.first, last: .user.name.last, email: .user.email}]" > ${FILEBASE}_filtered.json
+< ${FILEBASE}.json jq -r "[.results[] | {first: .user.name.first, last: .user.name.last, sex: .user.gender, email: .user.email, street: .user.location.street, city: .user.location.city, state: .user.location.state, zipcode: .user.location.zip}]" > ${FILEBASE}_filtered.json
 
 # Turn the JSON to flat csv
-< ${FILEBASE}_filtered.json json2csv -f first,last,email > ${FILEBASE}_filtered.csv
+< ${FILEBASE}_filtered.json json2csv -f first,last,sex,email,street,city,state,zipcode > ${FILEBASE}_filtered.csv
 
 # Look at what we got
 if [[ $CHATTY == "true" ]]; then
@@ -41,7 +41,7 @@ fi
 
 # Merge some columns with csvsql
 < ${FILEBASE}_filtered.csv csvsql \
-  --query "SELECT first || ' ' || last AS name, email FROM stdin" \
+  --query "SELECT first || ' ' || last AS name, sex, email, street, city, state, zipcode FROM stdin" \
   > ${FILEBASE}_filtered_merged.csv
 
 # Look at what we got
@@ -49,3 +49,6 @@ if [[ $CHATTY == "true" ]]; then
   csvlook ${FILEBASE}_filtered_merged.csv
 fi
 
+# Make a head file and a body file
+head -n 1 ${FILEBASE}_filtered_merged.csv > ${FILEBASE}_filtered_merged_header.csv
+cat ${FILEBASE}_filtered_merged.csv | header -d -n 1 > ${FILEBASE}_filtered_merged_body.csv
