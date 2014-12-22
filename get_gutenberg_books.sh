0a1d3293da18439059fbc7db34b9fe77a8b9317a
Gabriel Perdue <perdue@fnal.gov>
2014-12-22 16:21:17 -0600

    get more books

--- get_gutenberg_books.sh
+++ get_gutenberg_books.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-curl -s http://www.gutenberg.org/cache/epub/76/pg76.txt | head -n 10
-curl -s http://www.gutenberg.org/cache/epub/77/pg77.txt | head -n 10
+# curl -s http://www.gutenberg.org/cache/epub/76/pg76.txt | head -n 10
+# curl -s http://www.gutenberg.org/cache/epub/77/pg77.txt | head -n 10
 
-curl http://www.gutenberg.org/cache/epub/76/pg76.txt > finn.txt
+# curl http://www.gutenberg.org/cache/epub/76/pg76.txt > finn.txt
+# curl http://www.gutenberg.org/cache/epub/77/pg77.txt > sevengables.txt
+# curl http://www.gutenberg.org/cache/epub/78/pg78.txt > tarzan.txt
+# curl http://www.gutenberg.org/cache/epub/79/pg79.txt > termcompromise.txt
+# curl http://www.gutenberg.org/cache/epub/80/pg80.txt > onlineworld.txt
+# curl http://www.gutenberg.org/cache/epub/81/pg81.txt > returntarzan.txt
+curl http://www.gutenberg.org/cache/epub/100/pg100.txt > completeshakespeare.txt
+curl http://www.gutenberg.org/cache/epub/101/pg101.txt > hackercrackdown.txt
