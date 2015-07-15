1063a0d8129cd21b08a8537df68e4c03b7ddbf52
Gabriel Perdue <perdue@fnal.gov>
2015-07-15 09:42:28 -0500

    better comma splitter

--- comma_splitter.sh
+++ comma_splitter.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 ENERGYSTRING="1,9"
-ARR=$(echo $ENERGYSTRING | tr "," "\n")
-for x in $ARR
+echo "The raw string is $ENERGYSTRING"
+
+# replace commas with spaces, wrap in an array
+ARR=($(echo $ENERGYSTRING | tr "," " "))
+
+# get length
+ARRLEN=${#ARR[@]}
+echo "The split array length is $ARRLEN"
+
+if [[ $ARRLEN -gt 1 ]]; then
+  echo "The array length is greater than 1"
+fi
+
+echo "For loop:"
+for x in ${!ARR[@]}
 do
-  echo $x
+  echo "  Index $x = ${ARR[x]}"
 done
 
-echo ${ARR[0]}
-echo ${ARR[1]}
+echo "Direct access also: "
+echo "  Index 0 = ${ARR[0]}"
+echo "  Index 1 = ${ARR[1]}"
 
