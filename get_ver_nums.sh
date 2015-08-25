14052cf521e1435afc1ea9f2f298ce615883770b
Gabriel Perdue <perdue@fnal.gov>
2015-08-25 12:30:23 -0500

    import the future...

--- get_ver_nums.sh
+++ get_ver_nums.sh
@@ -6,6 +6,6 @@ echo $LHAPDFMAJOR
 BOOSTSRC=boost_1_56_0.tar.gz
 BOOSTVER=`echo $BOOSTSRC | cut -c7-12`
 echo $BOOSTVER
-BOOSTVER=`echo $BOOSTSRC | python -c "import sys;t=sys.stdin.readline().split('.')[0].split('_');print '%s.%s.%s'%(t[1],t[2],t[3])"`
+BOOSTVER=`echo $BOOSTSRC | python -c "from __future__ import print_function; import sys;t=sys.stdin.readline().split('.')[0].split('_');print('%s.%s.%s'%(t[1],t[2],t[3]))"`
 echo $BOOSTVER
 
