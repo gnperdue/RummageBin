19718803b908e929ee509d75baeb14150ab464ed
Gabriel Perdue <perdue@fnal.gov>
2015-08-27 11:42:39 -0500

    more one-liner splitting; add some perl in

--- get_ver_nums.sh
+++ get_ver_nums.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 LHAPDFSRC=LHAPDF-6.1.4.tar.gz
+GENIEVER=R-2_9_0
+BOOSTSRC=boost_1_56_0.tar.gz
+
+# bash `cut`
 LHAPDFMAJOR=`echo $LHAPDFSRC | cut -c8-8`
 echo $LHAPDFMAJOR
-BOOSTSRC=boost_1_56_0.tar.gz
+
 BOOSTVER=`echo $BOOSTSRC | cut -c7-12`
 echo $BOOSTVER
+
+
+# perl
+BOOSTVER=`echo $BOOSTSRC | perl -ne '@l=split("_",$_);print @l[2];'`
+echo $BOOSTVER
+
+GENIE_MAJOR=`echo $GENIEVER | perl -ne '@l=split("-",$_);@m=split("_",@l[1]);print @m[1];'`
+echo $GENIE_MAJOR
+
+
+# Python - this requires probably at least Python 2.6; it definitely fails on 2.4
 BOOSTVER=`echo $BOOSTSRC | python -c "from __future__ import print_function; import sys;t=sys.stdin.readline().split('.')[0].split('_');print('%s.%s.%s'%(t[1],t[2],t[3]))"`
 echo $BOOSTVER
+
 
