f2dd6db00f0e0009ccdc881fe61953c19c65a0a4
Gabriel Perdue <perdue@fnal.gov>
2015-10-26 17:26:50 -0500

    add some other things in comments

--- make_ten_kb_of_gibberish.sh
+++ make_ten_kb_of_gibberish.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 perl -le 'print map { ("a".."z", " ", "\n")[rand 28] } 1..10240' > ten_kb_file.dat
+
+# just 8 random numbers, etc.
+# perl -le 'print map { ("0".."9")[rand 10] } 1..8' > file.dat
