7216dfd1f61cdf2740cdfca92e7db0afa08ae45f
Gabriel Perdue <perdue@fnal.gov>
2016-07-19 18:31:32 -0500

    make the filename an arg

--- count_missing_subs.py
+++ count_missing_subs.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
-f = open('missing_me1A.txt', 'r')
+"""
+Count missing subruns per run.
+"""
+import sys
+
+if '-h' in sys.argv or '--help' in sys.argv:
+    print(__doc__)
+    sys.exit(1)
+
+if not len(sys.argv) == 2:
+    print('The missing subs file argument is mandatory.')
+    print(__doc__)
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+f = open(filename, 'r')
 lines = f.readlines()
 tot_missing_subs = 0
 for line in lines:
