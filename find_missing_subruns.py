e90b8c69f5c8da6d43667869c23cda35f05d3642
Gabriel Perdue <perdue@fnal.gov>
2016-08-02 16:05:05 -0500

    Add the option to supply a file (a playlist file) with runs and subruns instead of setting ranges. Also add the option to pass in a file prefix (first set of characters) so we can work with data easily (those files are not called 'SIM_minerva_...', etc.)

--- find_missing_subruns.py
+++ find_missing_subruns.py
@@ -2,17 +2,26 @@
 """
 take a root directory, a minumum run number, a maximum run number, a minimum
 subrun and number, and a maximum subrun number and report all missing sub
-runs between the min and max runs
-
-currently - look only for ROOT files in the pattern SIM_minerva_...Ana_Tuple;
-TODO: let the user pass in the lead part of the file string so this could
-work with other file types, etc.
+runs between the min and max runs. alternatively, supply a file with a list
+of runs and subruns (see below)
 
 Usage:
     python find_missing_subruns.py --root "/path/to/files" \
             --min_run 112202 --max_run 112205 --min_sub 1 --max_sub 5000
 
-We may supply a comma separated list for the `--root`.
+Or:
+    python find_missing_subruns.py --root "/path/to/files" \
+            --runs_file "/path/to/playlist"
+
+The `runs_file` or `playist` should be formatted like so:
+# RUN  SUBRUN,SUBRUN,...
+10068 1,2,3,4,5,6,7,8,9,10,11,12, ...
+10069 1,2,3,4,5,6,7,8,9,10, ...
+
+We may supply a comma separated list for the `--root`. We may also pass in
+a `tuple` to specify tuple type (default is 'Ana_Tuple'). We may also pass
+in a `file_prefix` to specify the leading characters in the file name
+(default is 'SIM_minerva' - good alternatives include 'MV', etc.).
 """
 from __future__ import print_function
 
@@ -33,33 +42,52 @@ if __name__ == '__main__':
 
     from optparse import OptionParser
     parser = OptionParser(usage=__doc__)
-    parser.add_option('--min_run', dest='min_run', default=112200,
+    parser.add_option('--min_run', dest='min_run', default=0,
                       help='Min run number', metavar='MIN_RUN',
                       type='int')
-    parser.add_option('--max_run', dest='max_run', default=112200,
+    parser.add_option('--max_run', dest='max_run', default=0,
                       help='Max run number', metavar='MAX_RUN',
                       type='int')
     parser.add_option('--min_sub', dest='min_sub', default=1,
                       help='Min sub number', metavar='MIN_SUB',
                       type='int')
-    parser.add_option('--max_sub', dest='max_sub', default=5000,
+    parser.add_option('--max_sub', dest='max_sub', default=1,
                       help='Max sub number', metavar='MAX_SUB',
                       type='int')
+    parser.add_option('--runs_file', dest='runs_file', type='string',
+                      default=None, help='Runs file', metavar='RUNS_FILE')
     parser.add_option('--root', dest='roots', type='string',
                       callback=arg_list_split, help='Root directory',
                       action='callback', metavar='ROOT_DIR')
+    parser.add_option('--file_prefix', dest='file_prefix',
+                      default='SIM_minerva',
+                      help='Ntuple prefix', metavar='NTUPLE_PREFIX')
     parser.add_option('--tuple', dest='tuple_type',
                       default='Ana_Tuple',
                       help='Ntuple type', metavar='TUPLE_TYPE')
     (options, args) = parser.parse_args()
 
     run_sub_dict = defaultdict(list)
-    for run in range(options.min_run, options.max_run + 1):
-        for sub in range(options.min_sub, options.max_sub + 1):
-            run_sub_dict[run].append(sub)
+    if options.runs_file is not None:
+        with open(options.runs_file, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if line[0] == '#':
+                    continue
+                run_subs = line.split()
+                if len(run_subs) < 2:
+                    continue
+                run = int(run_subs[0])
+                subs = run_subs[1].split(',')
+                for sub in subs:
+                    run_sub_dict[run].append(int(sub))
+    else:
+        for run in range(options.min_run, options.max_run + 1):
+            for sub in range(options.min_sub, options.max_sub + 1):
+                run_sub_dict[run].append(sub)
 
-    ana_tuple_file = re.compile(r'^SIM_minerva_(\d{8})_.*%s.*\.root$' %
-                                options.tuple_type)
+    ana_tuple_file = re.compile(r'^%s_(\d{8})_.*%s.*\.root$' %
+                                (options.file_prefix, options.tuple_type))
     four_digits_plus = re.compile(r'[0-9][0-9][0-9][0-9]+')
     roots = set(options.roots)
     for rootd in roots:
@@ -81,7 +109,8 @@ if __name__ == '__main__':
                             run_sub_dict[rn] = l
 
     for run, subs in run_sub_dict.items():
-        sys.stdout.write(str(run) + " ")
-        for sub in subs[:-1]:
-            sys.stdout.write(str(sub) + ",")
-        sys.stdout.write(str(subs[-1]) + "\n")
+        if len(subs) > 0:
+            sys.stdout.write(str(run) + " ")
+            for sub in subs[:-1]:
+                sys.stdout.write(str(sub) + ",")
+            sys.stdout.write(str(subs[-1]) + "\n")
