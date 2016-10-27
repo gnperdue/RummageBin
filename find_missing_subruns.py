a6374013a11197d7af16b2b23a3aee2a7dab6276
Gabriel Perdue <perdue@fnal.gov>
2016-10-27 09:28:57 -0500

    better (probably) error message

--- find_missing_subruns.py
+++ find_missing_subruns.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
                             try:
                                 l.remove(int(n))
                             except ValueError:
-                                print(n, "is not in the list")
+                                print("duplicate? " + str(root) + "/" + str(name))
                                 pass
                             run_sub_dict[rn] = l
 
