e10e5939a3200905cbb42b9b3d58d8c9a23ab82a
Gabriel Perdue <gnperdue@me.com>
2017-10-30 13:27:03 -0500

    singularity test script from J. Simone

---
+++ test_tf_singularity_img.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# script by J. Simone, FNAL
+
+singularity exec /data/simone/singularity/ubuntu16-cuda8-cudnn6-ml.img /bin/bash <<EOF
+cat /etc/issue
+nvidia-smi
+source /.singularity.d/environment
+python <<XEOF
+import tensorflow as tf
+# Creates a graph.
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
+XEOF
+EOF
